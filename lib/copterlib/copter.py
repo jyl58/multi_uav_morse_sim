896abf28dd02959c7bbb6ad78d981a2989e8bd7e
jiyingliang <jiyingliang369@126.com>
2019-01-02 13:15:05 +0800

    部件名字存在 使得无法正确仿真，暂时去掉

--- lib/copterlib/copter.py
+++ lib/copterlib/copter.py
@@ -8,21 +8,21 @@ class copter(object):
 		self.copter = Quadrotor()
 		self.sim_name=copter_name
 		self.copter.properties(Object = True, Graspable = False, Label = copter_name)
-		self.pose = Pose(name=copter_name+"_POS")
+		self.pose = Pose()
 		self.copter.append(self.pose)
 
-		self.imu = IMU(name=copter_name+"_IMU")
+		self.imu = IMU()
 		self.copter.append(self.imu)
 
-		self.gps = GPS(name=copter_name+"_GPS")
+		self.gps = GPS()
 		self.gps.alter('UTM')
 		self.copter.append(self.gps)
 
-		self.velocity = Velocity(name=copter_name+"_Velocity")
+		self.velocity = Velocity()
 		self.copter.append(self.velocity)
 
 		# create a compound sensor of all of the individual sensors and stream it
-		self.all_sensors = CompoundSensor([self.imu, self.gps, self.velocity, self.pose],name=copter_name+"_Sensors")
+		self.all_sensors = CompoundSensor([self.imu, self.gps, self.velocity, self.pose])
 		self.all_sensors.add_stream(link_protocol, port=60000)
 		self.copter.append(self.all_sensors)
 
@@ -34,12 +34,12 @@ class copter(object):
 		
 		# make the vehicle controllable via force and torque
 		# this will be available on port 4000 by default
-		self.engines = QuadrotorDynamicControl(name=copter_name+"_ENG")
+		self.engines = QuadrotorDynamicControl()
 		self.copter.append(self.engines)
 		self.engines.add_stream(link_protocol, port=60001)
 		
 	def get_copter(self):
-		return (self.copter)
+		return self.copter
 	
 	def get_sim_name(self):
 		return self.sim_name
