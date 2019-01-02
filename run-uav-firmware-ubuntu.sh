ad5309262100f222d34fd3b2961c4908d11dacf4
jiyingliang <jiyingliang369@126.com>
2019-01-02 15:45:42 +0800

    修改仿真启动脚本

--- run-uav-firmware-ubuntu.sh
+++ run-uav-firmware-ubuntu.sh
@@ -10,7 +10,13 @@ if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 
-echo "run the copter sim firmware"
+pid=$(ps -ef | grep "arducopter" | grep -v grep | awk '{print $2}')
+if [ -n "$pid" ]; then
+	echo "arducopter is already running , auto kill it "
+	kill "$pid"
+fi
+
+echo "run the copter sim firmware at background "
 ./firmware/bin/ubuntu/arducopter -S -I0 --home -35.363261,149.165230,584,353 --model morse-quad --speedup 1 --defaults ./firmware/uav-param/mav.parm 1>/dev/null & 
 
 if [[ $? -ne 0 ]]; then
@@ -19,4 +25,4 @@ if [[ $? -ne 0 ]]; then
 fi
 
 echo "run the mavproxy"
-mavproxy.py --master tcp:127.0.0.1:5760 --sitl 127.0.0.1:5501 --out 127.0.0.1:14550 --out 127.0.0.1:14551 
+mavproxy.py --master tcp:127.0.0.1:5760  --sitl 127.0.0.1:5501 --aircraft MyCopter --out 127.0.0.1:14550 --out 127.0.0.1:14551 
